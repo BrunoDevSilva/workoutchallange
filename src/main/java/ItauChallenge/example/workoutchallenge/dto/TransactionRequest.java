89cd533c077ffb2fef1dd727a57c5a33da3b56c6
BrunoPereira <bpsbrunopereira@gmail.com>
2025-03-08 18:20:04 -0300

    final challenge

--- src/main/java/ItauChallenge/example/workoutchallenge/dto/TransactionRequest.java
+++ src/main/java/ItauChallenge/example/workoutchallenge/dto/TransactionRequest.java
@@ -2,12 +2,11 @@ package ItauChallenge.example.workoutchallenge.dto;
 
 import java.time.OffsetDateTime;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class TransactionRequest {
     @NotNull
-    @Min(0)
+
     private Double valor;
 
     @NotNull
