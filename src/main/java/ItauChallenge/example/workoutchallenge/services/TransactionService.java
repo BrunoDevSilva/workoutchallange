2572f239513e9f10e3a32ab644b6f747440550fb
VcatDeveloper <110053118+VcatDeveloper@users.noreply.github.com>
2025-03-08 16:57:09 -0300

    services created

---
+++ src/main/java/ItauChallenge/example/workoutchallenge/services/TransactionService.java
@@ -0,0 +1,33 @@
+package ItauChallenge.example.workoutchallenge.services;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.springframework.stereotype.Service;
+
+import ItauChallenge.example.workoutchallenge.model.Transaction;
+
+@Service
+public class TransactionService {
+    
+    private final Queue <Transaction> transactions = new ConcurrentLinkedQueue<>();
+
+    public void addTransaction (Transaction transaction){
+        transactions.add(transaction);
+    }
+
+    public void clearTransactions(){
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics getStatistics(){
+        OffsetDateTime now =  OffsetDateTime.now();
+        return transactions.stream()
+                .filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValor)
+                .summaryStatistics();
+    }
+}
